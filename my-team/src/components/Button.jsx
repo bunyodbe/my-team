3a13327a398ace62c86ab5fda07a30da7e483c6f
bunyodbe <bsherazimov@gmail.com>
2022-12-11 14:04:30 +0500

    builded

--- my-team/src/components/Button.jsx
+++ my-team/src/components/Button.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Button = ({ content, color }) => (
   <button
     className={`py-[8px] px-[32px] font-livvic text-[18px] font-semibold transition duration-200 ease-in text-${color} leading-[28px] border-[2px] border-${color} rounded-[24px] `}
